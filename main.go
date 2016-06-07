5b956620a82c727ea8b8242f8543fc8adafa62d4
Centimitr <Centimitr@gmail.com>
2016-06-07 16:41:07 +0800

    update

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	cyako "github.com/Cyako/Cyako.go"
 	_ "github.com/Cyako/Cyako.go/realtime"
+	_ "github.com/Cyako/Cyako.go/statistics"
 
 	_ "github.com/Cyako/example/module"
 )
