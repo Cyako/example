25a30f82777f6eacb202a143bcf693052caf2cde
Centimitr <Centimitr@gmail.com>
2016-06-18 10:43:03 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # example
+
+[![Build Status](https://travis-ci.org/Cyako/example.svg?branch=master)](https://travis-ci.org/Cyako/example)
--- main.go
+++ main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	// framework
 	cyako "github.com/Cyako/Cyako.go"
-	_ "github.com/Cyako/Cyako.go/realtime"
-	_ "github.com/Cyako/Cyako.go/statistics"
 
+	// services
+	// _ "github.com/Cyako/Cyako.go/kvstore"
+	// _ "github.com/Cyako/Cyako.go/realtime"
+	// _ "github.com/Cyako/Cyako.go/specvalue"ss
+	// _ "github.com/Cyako/Cyako.go/jsonbase"
+	// _ "github.com/Cyako/Cyako.go/statistics"
+
+	// processor modules
 	_ "github.com/Cyako/example/module"
+
+	// system library
+	"fmt"
+	// "golang.org/x/net/websocket"
+	// "net/http"
 )
 
 func main() {
+
 	c := cyako.Ins()
-	c.Run(":3000", "/")
+	c.PrintLoadInfo()
+	// c.PrintAPIDoc()
+	c.CheckModule()
+	fmt.Println()
+
+	// SERVER
+	fmt.Println(" Running...")
+	fmt.Println()
+
+	err := c.Run(":3000", "/")
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
 }
